e068809547f812de07dddf631e9a67c965b1289a
James Palmer <jdpalmer@users.noreply.github.com>
2021-12-28 12:56:03 -0700

    Update transmute.rb

--- Formula/transmute.rb
+++ Formula/transmute.rb
@@ -1,1 +1,17 @@
+require "formula"
 
+class Transmute < Formula
+  desc "A small MacOS utility for doing image format conversions."
+  homepage "https://github.com/jdpalmer/transmute"
+  url "https://github.com/jdpalmer/transmute/archive/refs/tags/v1.3.zip"
+  sha256 "bfdecec1a8d94b8c5604f3d65fea475986f86fd05db61cf684f7b436ab7e30c8"
+  version "1.3"
+
+  def install
+    ENV['PREFIX'] = prefix
+    system "make"
+    bin.install "transmute"
+    man1.install "transmute.1"
+  end
+
+end
